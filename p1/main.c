c7a24712e8ec36c72eb84b782d0100a86ab21578
2022124058 <psyoun74@gmail.com>
2026-05-04 15:46:41 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N=0;
+    int A=0, B=0, C=0;
+    int found = 0;
+
+    scanf("%d", &N);
+
+    for (A = 1; A <= N / 900; A++) {
+        for (B = 2; B <= N / 750; B += 2) {
+            for (C = 1; C <= N / 200; C++) {
+
+                if (900 * A + 750 * B + 200 * C == N) {
+                    if (C < A || C < B) {
+                        printf("%d %d %d\n", A, B, C);
+                        found = 1;
+                    }
+                }
+
+            }
+        }
+    }
+
+    if (found == 0) {
+        printf("none\n");
+    }
+
+    return 0;
+}
+   
